6072a595997ae7d8c703f9ce2564a24ae5d08b9f
Admin <Admin@DESKTOP-8LCNEH3>
2025-08-26 14:54:33 +0530

    sample text

--- src/com/lbrce/cse/Test.java
+++ src/com/lbrce/cse/Test.java
@@ -2,6 +2,6 @@ package com.lbrce.cse;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello this is my first program");
+		System.out.println("Hello i am yaswanth");
 	}
 }
